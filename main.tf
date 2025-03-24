68707ede0ab1aeebf65b5ffa96fa3ccedcc55c81
Changsu Park <gd12091@gmail.com>
2025-03-24 10:28:21 +0900

    ✨ Feat: VPC

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
-#
+module "vpc" {
+  source = "./modules/vpc"
+
+  name_prefix = "myapp"
+  vpc_cidr = "10.0.0.0/16"
+  public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24"]
+  azs = ["ap-northeast-2a", "ap-northeast-2c"]
+}
