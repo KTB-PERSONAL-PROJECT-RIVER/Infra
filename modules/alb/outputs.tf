93fff5c3c93d2fa8cf18cfdf62ddf4f57125a1a6
Changsu Park <gd12091@gmail.com>
2025-03-24 17:28:35 +0900

    ✅ Add: outputs

--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -6,6 +6,10 @@ output "was_sg_id" {
   value = aws_security_group.was_sg.id
 }
 
+output "target_group" {
+  value = aws_lb_target_group.tg
+}
+
 output "target_group_arn" {
   value = aws_lb_target_group.tg.arn
 }
--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -5,3 +5,7 @@ output "iam_instance_profile_name" {
 output "iam_role_name" {
   value = aws_iam_role.ec2_role.name
 }
+
+output "iam_instance_profile_arn" {
+  value = aws_iam_instance_profile.ec2_profile.arn
+}
