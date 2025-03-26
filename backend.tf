82cae16a3d0cc4cb8f04038ef07df7b585d27305
Changsu Park <gd12091@gmail.com>
2025-03-26 10:29:24 +0900

    ✅ Update: terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "ktb-river-backend"
+    bucket         = "ktb-river-terraform-backend"
     key            = "dev/terraform.tfstate"
     region         = "ap-northeast-2"
     encrypt        = true
